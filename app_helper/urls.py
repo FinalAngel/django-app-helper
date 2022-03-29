1592e3e8ae169f7821ee84a3ca0bb3685ad83d81
Angelo Dini <angelo.dini@divio.ch>
2022-03-29 17:19:03 +0200

    fix urls

--- app_helper/urls.py
+++ app_helper/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import include, re_path as url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
